effa4857612def7a80c4358ce28cc2236f81de12
Teldoro <taldram375@gmail.com>
2023-05-03 21:05:17 +0300

    Add new features

--- Day 2/TipCalculator.py
+++ Day 2/TipCalculator.py
@@ -1,5 +1,3 @@
-print (round(33.6, 1))
-
 print("Welcome to the tip calculator")
 bill = input("What was the total bill?  ")
 percentage = input("What percentage tip would you like to give ? 10, 12, or 15? ")
@@ -8,7 +6,9 @@ people = input("How many people to split the bill? ")
 result_percentage = int(percentage)/100*int(bill)
 final_bill = int(bill)+int(result_percentage)
 split_bill = int(final_bill)/int(people)
+final_amount = split_bill
+final_amount = round(split_bill, 2)
+final_amount = "{:.2f}".format(split_bill)
 
 
-
-print(split_bill)
+print(f"Each person should pay: ${final_amount} ")
