47c7aaa14693da6e71add8525661a79729786cbf
Teldoro <taldram375@gmail.com>
2023-05-03 21:06:26 +0300

    Life in Weeks

---
+++ Day 2/Life in Weeks.py
@@ -0,0 +1,7 @@
+age = input ("What is your current age ?  ")
+
+result_days = (365 * 90)-(365 * int(age))
+result_weeks = (52*90)-(52*int(age))
+result_months = (12*90)-(12*int(age))
+
+print(f"You have {result_days} days, {result_weeks} weeks, and {result_months} until 90 years.")
