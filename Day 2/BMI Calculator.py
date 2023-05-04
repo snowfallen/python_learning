9747d3c519bb807fa45cfca411dd65f2bee28ee0
Teldoro <taldram375@gmail.com>
2023-05-04 12:44:36 +0300

    Fixed

--- Day 2/BMI Calculator.py
+++ Day 2/BMI Calculator.py
@@ -1,4 +1,4 @@
 height = input("enter your height in m: ")
-weight = input ("enter your weight in kg: ")
+weight = input("enter your weight in kg: ")
 result = int(weight) / (float(height) * (float(height)))
 print(int(result))
