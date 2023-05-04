7615ddddc309daac0c7bb5258e56448bf3810b99
Teldoro <taldram375@gmail.com>
2023-05-04 23:33:03 +0300

    Check Leap year programm

---
+++ Day 3/Leap_year.py
@@ -0,0 +1,11 @@
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    print("Leap year.")
+elif year % 100 == 0:
+    print("Not Leap year.")
+elif year % 400 == 0:
+    print("Leap year.")
+else:
+    print("Not Leap year.")
+    
