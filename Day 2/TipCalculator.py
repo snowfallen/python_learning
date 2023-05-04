d78a1200769d14b44bada50dcb0dba8842c3e32b
Teldoro <taldram375@gmail.com>
2023-05-04 11:54:32 +0300

    Fixed spaces and data type

--- Day 2/TipCalculator.py
+++ Day 2/TipCalculator.py
@@ -1,14 +1,14 @@
 print("Welcome to the tip calculator")
-bill = input("What was the total bill?  ")
-percentage = input("What percentage tip would you like to give ? 10, 12, or 15? ")
-people = input("How many people to split the bill? ")
+bill = int(input("What was the total bill?  "))
+percentage = int(input("What percentage tip would you like to give ? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
 
-result_percentage = int(percentage)/100*int(bill)
-final_bill = int(bill)+int(result_percentage)
-split_bill = int(final_bill)/int(people)
+result_percentage = percentage / 100 * bill
+final_bill = bill + result_percentage
+split_bill = final_bill / people
 final_amount = split_bill
-final_amount = round(split_bill, 2)
+final_amount = round(split_bill,2)
 final_amount = "{:.2f}".format(split_bill)
 
 
-print(f"Each person should pay: ${final_amount} ")
+print(f"Each person should pay: ${final_amount}")
