c5155d1476bdf78f2a0d51532e3ba174acd3a990
Teldoro <taldram375@gmail.com>
2023-05-04 14:55:06 +0300

    Fixed problem

--- Day 2/Datatypes.py
+++ Day 2/Datatypes.py
@@ -1,3 +1,2 @@
-digit_number = int(input("Type of two digit number:  ")) 
-print(digit_number[0] + digit_number[1])
-
+digit_number = input("Type of two digit number:  ")
+print(int(digit_number[0]) + int(digit_number[1]))
