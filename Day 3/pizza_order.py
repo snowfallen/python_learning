613e78594c6ffee15d114f5a3cc8c97b7f71092b
Teldoro <taldram375@gmail.com>
2023-05-06 13:01:53 +0300

    fixed spaces again

--- Day 3/pizza_order.py
+++ Day 3/pizza_order.py
@@ -1,8 +1,8 @@
 print("Welcome to Python Pizza Deliveries!")
 
-size = input("What size pizza do you want ? S, M or L ")
-add_pepperoni = input("Do you want pepperoni ? Y or N ")
-extra_cheese = input("Do you want extra cheese ? Y or N ")
+size = input("What size pizza do you want? S, M or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
 bill = 0
 
 if size == "S":
