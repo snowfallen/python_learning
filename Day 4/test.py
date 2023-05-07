3a935e30f9d941f1a9b19ff0896893d10a802d75
Teldoro <taldram375@gmail.com>
2023-05-07 19:58:29 +0300

    Add new files

---
+++ Day 4/test.py
@@ -0,0 +1,7 @@
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1][2])
+print(dirty_dozen[1][3])
