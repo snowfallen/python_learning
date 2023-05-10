416f5172230be9308151778eee38d106858ba5ed
snowfallen <bondarenkoov1998@gmail.com>
2023-05-10 15:13:14 +0200

    Add folder for LeetCode tasts solution

---
+++ LeetCode/two_sum.py
@@ -0,0 +1,18 @@
+from typing import List, Dict
+
+nums: List[int] = [2, 7, 11, 15]
+
+
+class Solution:
+    @staticmethod
+    def two_sum(number_list: List[int], target: int) -> List[int]:
+        hash_map: Dict[int] = {}
+        for index, number in enumerate(number_list):
+            hold: int = target - number
+            if number in hash_map:
+                return [hash_map[number], index]
+            else:
+                hash_map[hold] = index
+
+
+Solution.two_sum(nums, 9)
