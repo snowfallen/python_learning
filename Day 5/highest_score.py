f1c9cc5ea470f37df386da0ccc29a6aaf27f63a4
Teldoro <taldram375@gmail.com>
2023-05-11 15:02:24 +0300

    fixed

--- Day 5/highest_score.py
+++ Day 5/highest_score.py
@@ -1,6 +1,6 @@
 students_scores = input("Input a list of students scores ").split()
 
-for n in range (0,len(students_scores)):
+for n in range (0, len(students_scores)):
     students_scores[n] = int(students_scores[n])
 print(students_scores)
 
@@ -9,3 +9,4 @@ for score in students_scores:
     if score > highest_score:
         highest_score = score
 print(f"The highest score in the class is: {highest_score}")
+    
--- Day 5/pypassword_generator.py
+++ Day 5/pypassword_generator.py
@@ -4,11 +4,12 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 password_list = []
+
 
 for char in range (1, nr_letters + 1):
     password_list.append(random.choice(letters))
@@ -23,7 +24,11 @@ for num in range (1, nr_numbers + 1):
 
 
 random.shuffle(password_list)
-password = "".join(password_list)
+# password = "".join(password_list)
+password = ""
+for char in password_list:
+    password += char
+
 
 print(f"Your password is {password}")
 
