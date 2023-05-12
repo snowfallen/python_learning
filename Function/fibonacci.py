d710e59379f9b4e6fa1183b85103682568a85ee4
Teldoro <taldram375@gmail.com>
2023-05-12 19:55:23 +0300

    fix and add files

---
+++ Function/fibonacci.py
@@ -0,0 +1,12 @@
+number_list = []
+
+for index in range (0, 10):
+    if len(number_list) == 0:
+        number_list.append(1)
+    if len(number_list) == 1:
+        number_list.append(number_list[index] + number_list[index])
+    else:
+        number_list.append(number_list[index - 1] + number_list[index])
+        
+
+print(index)
