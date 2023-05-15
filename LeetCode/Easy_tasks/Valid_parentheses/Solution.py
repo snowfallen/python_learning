124811cb3bfb1eac0c0cd9fa61bfead077b39b88
snowfallen <bondarenkoov1998@gmail.com>
2023-05-15 10:14:45 +0200

    fix naming in valid parentheses task

--- LeetCode/Easy_tasks/Valid_parentheses/Solution.py
+++ LeetCode/Easy_tasks/Valid_parentheses/Solution.py
@@ -4,12 +4,12 @@ from typing import Dict, List
 class Solution:
     @staticmethod
     def is_valid(brackets_string: str) -> bool:
-        brackets_dictionary: Dict = {'(': ')', '[': ']', '{': '}'}
+        brackets_mapping: Dict[str, str] = {'(': ')', '[': ']', '{': '}'}
         stack: List[str] = []
         for bracket in brackets_string:
             if bracket in '([{':
                 stack.append(bracket)
-            elif len(stack) == 0 or bracket != brackets_dictionary[stack.pop()]:
+            elif len(stack) == 0 or bracket != brackets_mapping[stack.pop()]:
                 return False
         return len(stack) == 0
 
