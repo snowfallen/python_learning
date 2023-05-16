64a540983ec94cfe3944b0e9f859ec0fb914d7aa
snowfallen <bondarenkoov1998@gmail.com>
2023-05-16 15:43:32 +0200

    add new tast from leet code merge two sorted list (without solution yet)

---
+++ LeetCode/Easy_tasks/Merge_two_sorted_lists/LinkedList.py
@@ -0,0 +1,9 @@
+class ListNode:
+    def __init__(self, val: int = 0, next_node=None):
+        self.val: int = val
+        self.next = next_node
+
+
+newNode = ListNode(2)
+
+print(newNode)
---
+++ LeetCode/Easy_tasks/Merge_two_sorted_lists/Solution.py
@@ -0,0 +1,7 @@
+from typing import Optional
+
+from ListNode import ListNode
+
+
+class Solution:
+    def merge_two_lists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
